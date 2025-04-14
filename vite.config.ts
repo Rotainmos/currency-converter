9421509f236b1596887f5a6cc48caae64e250d0c
Nikolay Chekushkin <nikolay.chekushkin@ics-it.ru>
2025-04-14 17:27:28 +0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+	base: '/currency-converter/',
+	plugins: [vue(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 })
