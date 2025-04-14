0a7ea079eca4b4ca8486482590cf6e84bf92ae60
Nikolay Chekushkin <nikolay.chekushkin@ics-it.ru>
2025-04-14 18:15:48 +0500

    fix: 404.html support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs'
 
 export default defineConfig(({ command }) => ({
 	base: command === 'serve' ? '/' : '/currency-converter/',
-	plugins: [vue(), tailwindcss()],
+	plugins: [
+		vue(),
+		tailwindcss(),
+		{
+			name: 'copy-index-as-404',
+			closeBundle() {
+				fs.copyFileSync('dist/index.html', 'dist/404.html')
+			},
+		},
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
